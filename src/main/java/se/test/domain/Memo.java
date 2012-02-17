f5a10057aa54712415640f3f9dcf5a43ab041213
ped <duck_ComEn@hotmail.com>
2012-02-17 14:06:38 +0700

    create Memmo

---
+++ src/main/java/se/test/domain/Memo.java
@@ -0,0 +1,13 @@
+package se.test.domain;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Memo {
+
+    private String subject;
+}
